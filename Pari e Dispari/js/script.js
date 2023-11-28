b640b606eeeabd5af3744b59023b614d1a5a0463
DarioLoForti <lofortidario@gmail.com>
2023-11-28 14:58:50 +0100

    inserisco variabile per scelta numero da 1 a 5

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -3,3 +3,8 @@
 let pariDispari = prompt("inserisci se preferisci pari o dispari");
 console.log(pariDispari);
 
+// 2 - chiedo all'utende ti scegliere un numero tra 1 e 5
+
+let numberUser = parseInt(prompt("inserisci un numero da 1 a 5"));
+console.log(numberUser);
+console.log(pariDispari, numberUser);
